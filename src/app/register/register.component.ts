258956251a9fe316333fc852a2ca5080817fd696
Evan W <evwike@gmail.com>
2020-11-16 00:49:23 -0600

    Registration UI
    Built form and implemented limited validation for the Registration component.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registrationForm = new FormGroup({
+    first: new FormControl('', [Validators.required]),
+    last: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl(
+      '',
+      [
+        Validators.required,
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\\]).{8,32}$')
+      ]),
+    confirmPassword: new FormControl('', [Validators.required])
+  });
 
   constructor() { }
 
