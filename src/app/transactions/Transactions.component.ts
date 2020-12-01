5629c9cce1936040b01ae26a8c88563e35671392
DavidUMKC <davidjohnsongame@gmail.com>
2020-12-01 17:19:01 -0600

    Modified Transactions Component

--- src/app/transactions/Transactions.component.ts
+++ src/app/transactions/Transactions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { MatGridListModule} from '@angular/material/grid-list';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
 
 export interface Tile {
   Date: string;
@@ -14,18 +15,53 @@ export interface Tile {
 })
 
 export class AddTransactionsComponent implements OnInit{
-  constructor() { }
+  constructor(private addTransactionSheet: MatBottomSheet) { }
   ngOnInit(): void {
+  }
+  openTransactionSheet(): void{
+    this.addTransactionSheet.open(AddTransactionSheetComponent);
   }
 }
 
 @Component({
   selector: 'app-transactions-sheet',
-  templateUrl: '',
+  templateUrl: './addTransactionSheet.component.html'
 })
 
 export class AddTransactionSheetComponent implements OnInit {
-  constructor() {
-  }
+  constructor(private addTransactionSheetRef: MatBottomSheetRef<AddTransactionSheetComponent>) {}
   ngOnInit(): void {}
+  openLink(event: MouseEvent): void{
+    this.addTransactionSheetRef.dismiss();
+    event.preventDefault();
+  }
+}
+
+@Component({
+  selector: 'app-transactions',
+  templateUrl: './Transactions.html',
+  styleUrls: ['./Transactions.component.scss']
+})
+
+export class ExportTransactionsComponent implements OnInit{
+  constructor(private exportTransactionSheet: MatBottomSheet) { }
+  ngOnInit(): void {
+  }
+  openTransactionSheet(): void{
+    this.exportTransactionSheet.open(ExportTransactionSheetComponent);
+  }
+}
+
+@Component({
+  selector: 'app-transactions-sheet',
+  templateUrl: './exportTransactionSheet.component.html'
+})
+
+export class ExportTransactionSheetComponent implements OnInit {
+  constructor(private exportTransactionSheetRef: MatBottomSheetRef<ExportTransactionSheetComponent>) {}
+  ngOnInit(): void {}
+  openLink(event: MouseEvent): void{
+    this.exportTransactionSheetRef.dismiss();
+    event.preventDefault();
+  }
 }
