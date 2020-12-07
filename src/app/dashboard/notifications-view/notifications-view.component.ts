751ef0a97c42710ec797d3296582f004876e1d3a
Evan W <evwike@gmail.com>
2020-12-06 20:22:45 -0600

    Added new libraries, more responsive stuff

---
+++ src/app/dashboard/notifications-view/notifications-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-view',
+  templateUrl: './notifications-view.component.html',
+  styleUrls: ['./notifications-view.component.scss']
+})
+export class NotificationsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
