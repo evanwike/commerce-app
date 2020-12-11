458ff01dbc638dc22d6a66d720170a04c9c0cc46
AsharGit <32973667+AsharGit@users.noreply.github.com>
2020-12-11 14:49:35 -0600

    register update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../auth/auth.service';
-import {matchPassword} from '../password-validator.directive';
 
 @Component({
   selector: 'app-register',
@@ -10,6 +9,7 @@ import {matchPassword} from '../password-validator.directive';
 })
 
 export class RegisterComponent implements OnInit {
+  hide = true;
   registrationForm = new FormGroup({
     firstName: new FormControl('', [Validators.required]),
     lastName: new FormControl('', [Validators.required]),
@@ -17,8 +17,7 @@ export class RegisterComponent implements OnInit {
     password: new FormControl('', [Validators.required,
       Validators.minLength(8), Validators.pattern(/\d/),
       Validators.pattern(/[A-Z]/), Validators.pattern(/[a-z]/),
-      Validators.pattern(/[`~!@#$%^&*()_+={}|:";'<>?,./\-\[\]\\]/)]),
-    confirmPassword: new FormControl('', [Validators.required])
+      Validators.pattern(/[`~!@#$%^&*()_+={}|:";'<>?,./\-\[\]\\]/)])
   });
 
   constructor(
